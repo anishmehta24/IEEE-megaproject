011f4d42897de113d26c2b1e87a2d69991d01afe
Anish Mehta <mehtaanish111@gmail.com>
2025-01-12 23:41:42 +0530

    completed chat section

--- components/ui/custom/ChatView.jsx
+++ components/ui/custom/ChatView.jsx
@@ -7,7 +7,7 @@ import Lookup from '@/data/Lookup';
 import Prompt from '@/data/Prompt';
 import axios from 'axios';
 import { useConvex } from 'convex/react';
-import { ArrowRight, Link } from 'lucide-react';
+import { ArrowRight, Link, Loader2Icon } from 'lucide-react';
 import Image from 'next/image';
 import { useParams } from 'next/navigation'
 import React, { useContext, useEffect, useState } from 'react'
@@ -18,6 +18,7 @@ function ChatView() {
     const {messages,setMessages} = useContext(MessagesContext)
     const {userDetail,setUserDetail} = useContext(UserDetailContext)
     const [userInput,setUserInput] = useState()
+    const [loading,setLoading] = useState()
 
     useEffect(()=>{
         id && GetWorkspaceData()
@@ -41,6 +42,7 @@ function ChatView() {
      },[messages])
 
     const GetAiResponse=async()=>{
+        setLoading(true)
         const PROMPT = JSON.stringify(messages)+Prompt.CHAT_PROMPT
         const result = await axios.post('/api/ai-chat',{
             prompt:PROMPT 
@@ -50,6 +52,14 @@ function ChatView() {
             role:'ai',
             content:result.data.result
         }])
+        setLoading(false)
+    }
+
+    const onGenerate=(input)=>{
+        setMessages(prev=>[...prev,{
+            role:'user',
+            content:input
+        }])
     }
 
 
@@ -57,13 +67,20 @@ function ChatView() {
     <div className='relative h-[85vh] flex flex-col'>
         <div className='flex-1 overflow-y-scroll'>
             {messages?.map((msg,index)=>(
-                <div key={index} className='p-3 rounded-lg mb-2 flex gap-2' style={{
+                <div key={index} className='p-3 rounded-lg mb-2 flex gap-2 items-center leading-7' style={{
                     backgroundColor:Colors.CHAT_BACKGROUND
                 }}>
                     {msg?.role=='user' && <Image src={userDetail?.picture} alt='userImage' width={35} height={35} className='rounded-full'/>}
                     <h2>{msg.content}</h2>
+                   
                 </div>
             ))}
+            {loading && <div className='p-3 rounded-lg mb-2 flex gap-2 items-center' style={{
+                    backgroundColor:Colors.CHAT_BACKGROUND
+                }}>
+                        <Loader2Icon className='animate-spin'/>
+                        <h2>Generating Response...</h2>
+            </div>}
         </div>
 
         {/* Input section */}
