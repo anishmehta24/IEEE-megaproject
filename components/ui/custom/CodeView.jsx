08804048d5a9de3050f54edc4e6735dec49204d3
Anish Mehta <mehtaanish111@gmail.com>
2025-01-13 01:07:11 +0530

    created code editor frontend

--- components/ui/custom/CodeView.jsx
+++ components/ui/custom/CodeView.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
+import Lookup from '@/data/Lookup';
 
 function CodeView() {
+  const [activeTab, setActiveTab] = useState('code');
+  
   return (
-    <div>CodeView</div>
+    <div>
+      <div className='bg-[#181818] w-full p-2 border'>
+        <div className='flex items-center flex-wrap shrink-0 bg-black p-1 justify-center w-[140px] gap-3 rounded-full' >
+          <h2 className={`text-sm cursor-pointer ${activeTab=='code' && 'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full'}`}
+            onClick={()=>setActiveTab('code')}
+          >Code</h2>
+          <h2 className={`text-sm cursor-pointer ${activeTab=='preview' && 'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full'}`}
+               onClick={()=>setActiveTab('preview')}
+          >Preview</h2>
+          
+        </div>
+      </div>
+      <SandpackProvider template="react" theme={'dark'}
+        customSetup={{
+          dependencies:{
+            ...Lookup.DEPENDANCY
+          }
+        }}
+      >
+        <SandpackLayout>
+          {activeTab=='code'?<> 
+          <SandpackFileExplorer />
+          <SandpackCodeEditor />
+          </>:
+          <>
+          <SandpackPreview showNavigator={true}/>
+          </>}
+        </SandpackLayout>
+      </SandpackProvider>
+    </div>
   )
 }
 
